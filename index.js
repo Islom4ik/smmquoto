3911ab0a7c2046893c6c9c2c8bcdddbc4c6bc91d
Islom4ik <muxammadalixanov9@gmail.com>
2023-01-22 00:04:37 +0500

    new

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+app.use(router);
+app.set('view engine', 'ejs');
+
+app.use('/', function (request, response) {
+    response.render('index')
+})
+
+router.get('/lava_verify=KP9DQwAat8XBfMiUm6GST2k0joNlgeHrc7xYRzIv3Wp1Cu4n', function (request, response) {
+    response.render('main.ejs')
+})
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+});
+
